c963dbfb6e23659c137f68acf0c6d2fa2273e5eb
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-04-15 18:01:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ this.age = age;
 
 Person.prototype.greet = function () {
 
-console.log(Hello, my name is ${this.name} and I am ${this.age} years old.);
+console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 
 };
 
@@ -26,10 +26,7 @@ Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function () {
 
-console.log(
-
-`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-);
+console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 
 };
 
